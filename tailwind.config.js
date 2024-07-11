b8cd7546115f5083b82cebd1e9031554fca00aa8
unknown <NATHALIE15279>
2024-07-11 10:21:33 +0200

    update(11-07-2024)011

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./**/*.{html,js}"], content: [],
+  content: ["./src/**/*.{html,js}", "./maquet/*.{html,js}", "./**/*.{html,js}"],
+  content: [],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
